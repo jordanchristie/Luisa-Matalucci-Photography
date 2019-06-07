9016c9052d8a40ea98a5f5ef4b0ce03a111a214b
Jordan Christie <jordanchristie116@gmail.com>
2019-06-07 12:39:20 -0400

    removed text decoration from gallery

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
